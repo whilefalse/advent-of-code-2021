50b9841983573ef79b603ae03026e37d33031834
Steven Anderson <steve@whilefalse.net>
2021-12-01 20:08:44 +0000

    Day 1 Part 2

--- crates/day1/src/main.rs
+++ crates/day1/src/main.rs
@@ -3,7 +3,7 @@ const INPUT: &str = include_str!("input.txt");
 fn main() {
     // println!("input: {:?}", INPUT);
     println!("problem1: {}", solve_problem1(INPUT));
-    // println!("problem2: {}", solve_problem2(INPUT));
+    println!("problem2: {}", solve_problem2(INPUT));
 }
 
 #[allow(unused)]
@@ -17,7 +17,17 @@ fn solve_problem1(input: &str) -> i32 {
 
 #[allow(unused)]
 fn solve_problem2(input: &str) -> i32 {
-    todo!()
+    let ns = numbers(input);
+    let sums = ns
+        .iter()
+        .zip(ns.iter().skip(1))
+        .zip(ns.iter().skip(2))
+        .map(|((one, two), three)| one + two + three)
+        .collect::<Vec<_>>();
+    sums.iter()
+        .zip(sums.iter().skip(1))
+        .filter(|(one, two)| two > one)
+        .count() as i32
 }
 
 fn numbers(input: &str) -> Vec<i32> {
