b2edc230bebcb11957e54a06bc3587e7609a0221
Steven Anderson <steve@whilefalse.net>
2021-12-12 08:18:57 +0000

    Day 12 speed ups

--- crates/day12/src/main.rs
+++ crates/day12/src/main.rs
@@ -4,31 +4,30 @@ use std::time::Instant;
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
-    println!("problem1: {}", solve_problem1(INPUT));
-    println!("problem2: {}", solve_problem2(INPUT));
+    let now = Instant::now();
+    println!("Part 1: {}", solve_problem1(INPUT));
+    println!("Part 2 {}", solve_problem2(INPUT));
+    println!("Time {:?}", now.elapsed());
 }
 
 #[allow(unused)]
 fn solve_problem1(input: &str) -> u32 {
     let parsed = parse(input);
-    let paths = find_all_paths(&parsed, &mut vec!["start"], false, false);
+    let paths = find_all_paths(&parsed, &mut vec!["start"], true);
     paths
 }
 
 #[allow(unused)]
 fn solve_problem2(input: &str) -> u32 {
-    let now = Instant::now();
     let parsed = parse(input);
-    let paths = find_all_paths(&parsed, &mut vec!["start"], true, false);
-    println!("took {:?}", now.elapsed());
+    let paths = find_all_paths(&parsed, &mut vec!["start"], false);
     paths
 }
 
 fn find_all_paths<'a>(
     hash: &HashMap<&'a str, Vec<&'a str>>,
     current: &mut Vec<&'a str>,
-    can_retread_small_cave: bool,
-    has_retrod_small_cave: bool,
+    retrod: bool,
 ) -> u32 {
     let current_head = current[current.len() - 1];
     if current_head == "end" {
@@ -38,18 +37,11 @@ fn find_all_paths<'a>(
         .iter()
         .map(|conn| {
             let retreading = is_small_cave(conn) && current.contains(conn);
-            if (retreading && (has_retrod_small_cave || !can_retread_small_cave))
-                || *conn == "start"
-            {
+            if (retreading && retrod) || *conn == "start" {
                 0
             } else {
                 current.push(conn);
-                let found = find_all_paths(
-                    hash,
-                    current,
-                    can_retread_small_cave,
-                    has_retrod_small_cave || retreading,
-                );
+                let found = find_all_paths(hash, current, retrod || retreading);
                 current.pop();
                 found
             }
@@ -58,15 +50,13 @@ fn find_all_paths<'a>(
 }
 
 fn is_small_cave(cave: &str) -> bool {
-    cave.to_ascii_lowercase() == cave
+    cave.chars().all(|c| c.is_lowercase())
 }
 
 fn parse<'a>(input: &'a str) -> HashMap<&'a str, Vec<&'a str>> {
     let mut hash = HashMap::new();
     for line in input.lines() {
-        let mut split = line.split("-");
-        let from = split.next().unwrap();
-        let to = split.next().unwrap();
+        let (from, to) = line.split_once("-").unwrap();
         hash.entry(from).or_insert(vec![]).push(to);
         hash.entry(to).or_insert(vec![]).push(from);
     }
--- crates/template/src/main.rs
+++ crates/template/src/main.rs
@@ -1,9 +1,13 @@
+use std::time::Instant;
+
 const INPUT: &str = include_str!("input.txt");
 
 fn main() {
+    let now = Instant::now();
     println!("input: {:?}", INPUT);
-    // println!("problem1: {}", solve_problem1(INPUT));
-    // println!("problem2: {}", solve_problem2(INPUT));
+    // println!("Part 1: {}", solve_problem1(INPUT));
+    // println!("Part 2: {}", solve_problem2(INPUT));
+    println!("Time {:?}", now.elapsed());
 }
 
 #[allow(unused)]
@@ -13,5 +17,10 @@ fn solve_problem1(input: &str) -> i32 {
 
 #[allow(unused)]
 fn solve_problem2(input: &str) -> i32 {
+    todo!()
+}
+
+#[allow(unused)]
+fn parse(input: &str) {
     todo!()
 }
